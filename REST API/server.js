8dbb0249812a0ec7b68ffcb8130ea74c2904390f
K-Junyyy <oasis21hd@gmail.com>
2021-10-26 00:59:32 +0900

    ADD

--- REST API/server.js
+++ REST API/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const server = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "variables.env" });
+const User = require("./models/User");
 
 server.use(bodyParser.json());
 
@@ -17,8 +21,39 @@ const users = [
   },
 ];
 
+server.get("/", (req, res) => {
+  const newUser = new User();
+  newUser.email = "kjy1787@gmail.com";
+  newUser.name = "cocoon";
+  newUser.age = 25;
+  newUser
+    .save()
+    .then((user) => {
+      console.log(user);
+      res.json({
+        message: "User created Successfully",
+      });
+    })
+    .catch((err) => {
+      res.json({
+        message: "User was not created Successfully",
+      });
+    });
+});
+
 server.get("/api/user", (req, res) => {
   res.json(users);
+});
+
+server.get("/api/user/:id", (req, res) => {
+  const user = users.find((u) => {
+    return u.id === req.params.id;
+  });
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ errorMessage: "User was not found" });
+  }
 });
 
 server.post("/api/user", (req, res) => {
@@ -26,6 +61,41 @@ server.post("/api/user", (req, res) => {
   res.json(users);
 });
 
-server.listen(3000, () => {
-  console.log("server is running");
+server.put("/api/user/:id", (req, res) => {
+  let foundIndex = users.findIndex((u) => u.id === req.params.id);
+  if (foundIndex === -1) {
+    res.status(404).json({ errorMessage: "User was not found" });
+  } else {
+    users[foundIndex] = { ...users[foundIndex], ...req.body };
+    res.json(users[foundIndex]);
+  }
+});
+
+server.delete("/api/user/:id", (req, res) => {
+  let foundIndex = users.findIndex((u) => u.id === req.params.id);
+  if (foundIndex === -1) {
+    res.status(404).json({ errorMessage: "User was not found" });
+  } else {
+    let foundUser = users.splice(foundIndex, 1);
+    res.json(foundUser[0]);
+  }
+});
+
+server.listen(3000, (err) => {
+  if (err) {
+    console.log("server is error");
+  } else {
+    console.log("server is running");
+    mongoose.connect(
+      process.env.MONGODB_URL,
+      { useNewUrlParser: true },
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Connected to database successfulyy");
+        }
+      }
+    );
+  }
 });
